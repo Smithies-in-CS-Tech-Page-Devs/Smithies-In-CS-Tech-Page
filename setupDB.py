1e8dab7a6eb2e851713e6499aaf9335927f27b5e
elainoz <72514388+elainoz@users.noreply.github.com>
2021-01-08 17:16:09 -0800

    setup db file

---
+++ setupDB.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy as fsqla
+
+Base = fsqla.declarative_base()
+
+class Conferences(Base):
+    # assign table names
+    __tablename__ = 'Women in Tech Conferences'
+    __table_args__ = {'sqlite_autoincrement': True}
+
+
+
+
+
+
