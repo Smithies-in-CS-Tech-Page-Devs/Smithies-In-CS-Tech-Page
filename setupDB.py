3c431cae5476cf4fe2db0be87375dd5a907a1561
elainezhang <ezzhang@smith.edu>
2021-01-10 18:42:40 -0800

    setting up google api

--- setupDB.py
+++ setupDB.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy as fsqla
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy as fsqla
 
 # Base = fsqla.declarative_base()
 #
@@ -8,6 +8,13 @@ from flask_sqlalchemy import SQLAlchemy as fsqla
 #     __tablename__ = 'Women in Tech Conferences'
 #     __table_args__ = {'sqlite_autoincrement': True}
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds​", "https://www.googleapis.com/auth/spreadsheets​","https://www.googleapis.com/auth/drive.file​","https://www.googleapis.com/auth/drive​"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open("Women").sheet1
 
 
 
