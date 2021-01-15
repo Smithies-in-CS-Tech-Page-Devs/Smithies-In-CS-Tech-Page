5f10a43d0a5f23273bedb18dbd2b84ce47efb687
JC <jc@JCs-MacBook-Pro.fios-router.home>
2021-01-15 02:20:44 -0500

    #19 Initial landing page with navbar

--- scs-app/src/app/home/home.component.ts
+++ scs-app/src/app/home/home.component.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    styleUrls: ['./home.component.scss',
+    '../app.component.scss'],
+    animations:[
+        trigger('fade', [ 
+            transition('void => *', [
+              style({ opacity: 0 }), 
+              animate("0s 2s", style({opacity: 1}))
+            ]) 
+        ])
+    ]
 })
 
 export class HomeComponent implements OnInit {
