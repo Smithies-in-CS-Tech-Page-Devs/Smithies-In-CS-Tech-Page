8343102262989d97345bd141effe67fb4c8f20d7
Lillian <lillianj@smith.edu>
2021-01-20 15:30:35 +0800

    #10 connect flask with angular

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from flask import Flask
+from flask import send_from_directory
+from werkzeug.routing import BaseConverter
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Index Page'
+app = Flask(__name__, static_url_path="", static_folder="scs-app/dist/scs-app/")
 
-@app.route('/hello')
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+
+app.url_map.converters['regex'] = RegexConverter
+
+@app.route("/")
+def angular():
+    return send_from_directory("scs-app/dist/scs-app/", "index.html")
+
+@app.route("/<regex('\w\.(js|css)'):path>")
+def angular_src(path):
+    return send_from_directory("scs-app/dist/scs-app/", path)
+
+@app.route("/hello")
 def hello():
-    return 'Hello, World'
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
