56eab2ff4f72549849568031b908143de551465a
elainezhang <ezzhang@smith.edu>
2021-01-30 17:37:23 -0800

    add search bar

--- scs-app/src/app/app.module.ts
+++ scs-app/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TechResourcesComponent } from './tech-resources/tech-resources.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
+    TechResourcesComponent,
   ],
   imports: [
     BrowserModule,
---
+++ scs-app/src/app/tech-resources/tech-resources.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tech-resources',
+  templateUrl: './tech-resources.component.html',
+  styleUrls: ['./tech-resources.component.scss']
+})
+export class TechResourcesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
